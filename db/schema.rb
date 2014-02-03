5fc51b6e039983948a304f58bcd1887779e9385b
Ralf Nieuwenhuijsen <ralf@devstar.ra-ge.net>
2014-02-03 07:59:28 -0500

    initial database dump

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140202103924) do
+ActiveRecord::Schema.define(version: 20140202134414) do
 
   create_table "extmaps", force: true do |t|
     t.string   "suffix"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20140202103924) do
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "preview_url"
   end
 
   create_table "skins", force: true do |t|
