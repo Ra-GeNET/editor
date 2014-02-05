d3c0b35b95e5bd9506c1040559c34b1a9afb8bcc
Ralf Nieuwenhuijsen <ralf.nieuwenhuijsen@gmail.com>
2014-02-05 05:15:06 -0500

    - make refresh smarter

--- app/models/site.rb
+++ app/models/site.rb
@@ -3,13 +3,19 @@ class Site < ActiveRecord::Base
   has_many :fiddles  
   
   def refresh
-    # remove current fiddles from the database
-    Fiddle.where( :site_id => self.id ).each { |f| f.destroy }
     # inject all files again
     fx = Dir.glob("#{self.file_path}/**/*").reject { |file_path| File.directory? file_path }   
     fx.each do |fn|
       Fiddle.load_from fn, self
     end        
+
+    # remove current fiddles from the database
+    Fiddle.where( :site_id => self.id ).each do |f|
+      unless fx.include?( self.file_path + f.file_path )  then
+        f.destroy
+      end
+    end
+   
  end 
  
   
